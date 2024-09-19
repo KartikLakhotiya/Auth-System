c148bb885ea3761c5655251baac2d3fcc6eb79fa
Kartik  Lakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-09-19 10:41:38 +0530

    modified logout route to fix cokies issue

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -5,6 +5,8 @@ export const generateTokenAndSetCookie = async (res, userId) => {
         expiresIn: "7d"
     })
 
+    // localStorage.setItem('token', token);
+
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
